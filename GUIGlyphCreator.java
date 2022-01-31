5c4e89692790d4b21c8ed3dc8cd308991ab1199c
tqamarVT <53414514+tqamarVT@users.noreply.github.com>
2022-01-30 19:17:43 -0500

    Update GUIGlyphCreator.java

--- GUIGlyphCreator.java
+++ GUIGlyphCreator.java
@@ -1,21 +1,15 @@
-// Virginia Tech Honor Code Pledge:
-//
-// As a Hokie, I will conduct myself with honor and integrity at all times.
-// I will not lie, cheat, or steal, nor will I accept the actions of those
-// who do.
-//
-
 package prj5;
-
 import java.awt.Color;
 import CS2114.*;
 
 /**
  * Creates the class for the GlyphCreator
  * 
- * @author Caroline Hoang
- * @version 2019.08.15
+ * @author Taimoor Qamar
+ * @version 2019.13.08
  */
+ */
+     
 public class GUIGlyphCreator {
 
     private TextShape text;
