9a0abafa1bc292894a1c0665ed025e4e9c17655f
tqamarVT <53414514+tqamarVT@users.noreply.github.com>
2022-01-30 19:16:05 -0500

    Update GUIDisplay.java

--- GUIDisplay.java
+++ GUIDisplay.java
@@ -1,9 +1,7 @@
-// Virginia Tech Honor Code Pledge:
-//
-// As a Hokie, I will conduct myself with honor and integrity at all times.
-// I will not lie, cheat, or steal, nor will I accept the actions of those
-// who do.
-// -- Caroline Hoang (hoangcd)
+/*
+ * @author Taimoor Qamar
+ * @version 2019.13.08
+ */
 
 package prj5;
 /**
