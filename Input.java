5a98b2decd13933b8728c249ac79005e536219b3
tqamarVT <53414514+tqamarVT@users.noreply.github.com>
2022-01-30 19:19:02 -0500

    Update Input.java

--- Input.java
+++ Input.java
@@ -1,18 +1,10 @@
-// Virginia Tech Honor Code Pledge:
-//
-// As a Hokie, I will conduct myself with honor and integrity at all times.
-// I will not lie, cheat, or steal, nor will I accept the actions of those
-// who do.
-//
-
 package prj5;
-
 import java.io.FileNotFoundException;
 
 /**
  * Runs the GUIdisplay
- * 
- * @author Caroline Hoang
+ * @author Taimoor Qamar
+ * @version 2019.13.08
  *
  */
 public class Input {
